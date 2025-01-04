2d788b48fa57d5c9f8ba0187e5b5eed8bb45788c
AYMAN-KHAN-000 <aymanuchiha99@gmail.com>
2025-01-04 20:54:58 +0600

    tail pro

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: "class",
+  content: ["*"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['"Plus Jakarta Sans"', 'sans-serif'],
+        inter: ['"Inter"', 'serif'], // Added Inter font here
+      },
+    },
+  },
+  plugins: [],
+}
